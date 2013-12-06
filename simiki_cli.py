feb094c331d9eda8b69cc141512984a49a220d89
Tanky Woo <wtq1990@gmail.com>
2013-12-06 17:23:57 +0800

    change function to class

--- simiki_cli.py
+++ simiki_cli.py
@@ -8,7 +8,6 @@ Usage:
   simiki_cli build_site
   simiki_cli new_wiki -c <catalog> -t <title> [-f <file>]
   simiki_cli generate
-  simiki_cli preview
   simiki_cli -h | --help
   simiki_cli -V | --version
 
@@ -21,15 +20,17 @@ Options:
 
 """
 
+# @TODO
+# simiki_cli preview
 
 import os
 import sys
 import codecs
 import datetime
 import shutil
-import errno
 from os import path as osp
 
+import yaml
 from docopt import docopt
 
 BASE_DIR = osp.dirname(osp.realpath(__file__))
@@ -37,98 +38,107 @@ if BASE_DIR not in sys.path:
     sys.path.insert(0, BASE_DIR)
 
 import simiki.utils
-import simiki.configs
+from simiki.configs import parse_configs
 import simiki.generators
 
+class Simiki(object):
 
-def copyanything(src, dst):
-    try:
-        shutil.copytree(src, dst)
-    except OSError as exc: # python >2.5
-        if exc.errno == errno.ENOTDIR:
-            shutil.copy(src, dst)
-        else: raise
+    def __init__(self, configs):
+        self.configs = configs
 
-def build_site():
-    content_path = simiki.configs.CONTENT_PATH
-    output_path = simiki.configs.OUTPUT_PATH
-    for path in (content_path, output_path):
-        if osp.exists(path):
-            print(simiki.utils.color_msg("info", "[%s] exists." % path))
-        else:
-            os.mkdir(path)
+    def update_css(self):
+        print(simiki.utils.color_msg(
+            "info", "Update theme [{0}] css.".format(self.configs["theme"])))
+
+        css_src = osp.join(self.configs["tpl_path"], "css")
+        css_dst = osp.join(self.configs["destination"], "css")
+        if osp.exists(css_dst):
+            shutil.rmtree(css_dst)
+
+        simiki.utils.copytree(css_src, css_dst)
+
+    def build_site(self):
+        content_path = self.configs["source"]
+        output_path = self.configs["destination"]
+        for path in (content_path, output_path):
+            if osp.exists(path):
+                print(simiki.utils.color_msg("info", "[%s] exists." % path))
+            else:
+                os.mkdir(path)
+                print(simiki.utils.color_msg(
+                    "info", "create directory [%s]." % path))
+
+        self.update_css()
+
+    def create_new_wiki(self, catalog, filename, title, date, layout="post"):
+        meta = "\n".join([
+            "---",
+            "layout: {}".format(layout),
+            "title: \"{}\"".format(title),
+            "date: {}".format(date),
+            "---",
+        ])
+        meta += "\n\n\n"
+
+        catalog_path = osp.join(self.configs["destination"], catalog)
+        if not simiki.utils.check_path_exists(catalog_path):
+            os.mkdir(catalog_path)
             print(simiki.utils.color_msg(
-                "info", "create directory [%s]." % path))
-    # TODO cp css
-    #copyanything(osp.join(simiki.configs.TPL_PATH, "style.css"), simiki.configs.OUTPUT_PATH)
+                "info", "create catalog [%s]." % catalog))
 
-def create_new_wiki(catalog, filename, title, date, layout="post"):
-    meta = "\n".join([
-        "---",
-        "layout: {}".format(layout),
-        "title: \"{}\"".format(title),
-        "date: {}".format(date),
-        "---",
-    ])
-    meta += "\n\n\n"
+        fn = osp.join(catalog_path, filename)
+        if simiki.utils.check_path_exists(fn):
+            print(simiki.utils.color_msg(
+                "warning", "wiki file exists: {}".format(fn)
+            ))
+        else:
+            print(simiki.utils.color_msg(
+                "info", "create new wiki: {}".format(fn)
+            ))
+            with codecs.open(fn, "wb", "utf-8") as fd:
+                fd.write(meta)
 
-    catalog_path = osp.join(simiki.configs.CONTENT_PATH, catalog)
-    if not simiki.utils.check_path_exists(catalog_path):
-        os.mkdir(catalog_path)
-        print(simiki.utils.color_msg(
-            "info", "create catalog [%s]." % catalog))
+    def generate_single_page(self, md_file):
+        pgen = simiki.generators.PageGenerator(self.configs, md_file)
+        html = pgen.mdown2html()
+        pgen.output_to_file(html)
 
-    fn = osp.join(catalog_path, filename)
-    if simiki.utils.check_path_exists(fn):
-        print(simiki.utils.color_msg(
-            "warning", "wiki file exists: {}".format(fn)
-        ))
-    else:
-        print(simiki.utils.color_msg(
-            "info", "create new wiki: {}".format(fn)
-        ))
-        with codecs.open(fn, "wb", "utf-8") as fd:
-            fd.write(meta)
+    def generate_all_pages(self):
+        content_path = self.configs["source"]
 
-def generate_single_page(md_file):
-    pgen = simiki.generators.PageGenerator(md_file)
-    html = pgen.mdown2html()
-    pgen.output_to_file(html)
+        for root, dirs, files in os.walk(content_path):
+            for filename in files:
+                if not simiki.utils.check_extension(filename):
+                    continue
+                md_file = osp.join(root, filename)
+                self.generate_single_page(md_file)
 
-def generate_all_pages():
-    content_path = simiki.configs.CONTENT_PATH
+    def generate_catalog(self):
+        cgen = simiki.generators.CatalogGenerator(
+                self.configs,
+                self.configs["base_dir"],
+                self.configs["source"], 
+                self.configs["destination"])
+        cgen.update_catalog_page()
 
-    for root, dirs, files in os.walk(content_path):
-        for filename in files:
-            if not simiki.utils.check_extension(filename):
-                continue
-            md_file = osp.join(root, filename)
-            generate_single_page(md_file)
-
-def generate_catalog():
-    cgen = simiki.generators.CatalogGenerator(
-            simiki.configs.BASE_DIR,
-            simiki.configs.CONTENT_PATH, 
-            simiki.configs.OUTPUT_PATH)
-    cgen.update_catalog_page()
-
-def generate():
-    generate_all_pages()
-    generate_catalog()
+    def generate(self):
+        self.generate_all_pages()
+        self.generate_catalog()
 
 def main():
+    # TODO
+    configs = parse_configs("_config.yml")
+    simiki = Simiki(configs)
     args = docopt(__doc__, version='Simiki 0.1')
     if args["build_site"]:
-        build_site()
+        simiki.build_site()
     elif args["generate"]:
-        generate()
-    elif args["preview"]:
-        pass
+        simiki.generate()
     elif args["-c"] and args["-t"]:
         if not args["-f"]:
             args["-f"] = "{}.md".format("-".join(args["-t"].split()).lower())
         cur_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
-        create_new_wiki(
+        simiki.create_new_wiki(
             args["-c"], 
             args["-f"], 
             args["-t"], 
