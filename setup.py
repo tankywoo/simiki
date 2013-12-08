71917f0cb9216074281186b6c58805ba32cafa76
Tanky Woo <wtq1990@gmail.com>
2013-12-08 18:13:37 +0800

    for setuptools

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+entry_points = {
+    "console_scripts": [
+        "simiki = simiki.cli:main",
+    ]
+}
+
+requires = [
+    "Markdown",
+    "Pygments",
+    "Jinja2",
+    "PyYAML",
+    "docopt",
+]
+
+setup(
+    name = "simiki",
+    version = "0.1.0",
+    url = "https://github.com/tankywoo/simiki",
+    author = "Tanky Woo",
+    author_email = "me@tankywoo.com",
+    description = "Simiki is a simple static site generator for wiki.",
+    license = "MIT License",
+    packages = find_packages(),
+    include_package_data=True,
+    install_requires = requires,
+    entry_points = entry_points,
+)
