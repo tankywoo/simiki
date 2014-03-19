628da3c229854f533ed826cf7c2d87d13a3cc285
Tanky Woo <wtq1990@gmail.com>
2014-03-19 09:54:49 +0800

    release v0.2.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ requires = [
 
 setup(
     name = "simiki",
-    version = "0.1.0",
+    version = "0.2.0",
     url = "https://github.com/tankywoo/simiki",
     author = "Tanky Woo",
     author_email = "me@tankywoo.com",
--- simiki/__init__.py
+++ simiki/__init__.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
