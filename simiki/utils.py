4dad841bab6ba8f2c1044a2804306fddd7f73a28
Tanky Woo <wtq1990@gmail.com>
2014-05-05 01:30:09 +0800

    add mkdir_p for make parent directory, like linux mkdir -p

--- simiki/utils.py
+++ simiki/utils.py
@@ -87,6 +87,15 @@ def emptytree(directory):
         else:
             logger.error("Unable to delete %s, unknown filetype" % fp)
 
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc: # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else: raise
+
+
 if __name__ == "__main__":
     print(color_msg("black", "Black"))
     print(color_msg("red", "Red"))
