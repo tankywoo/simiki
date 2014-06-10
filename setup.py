73edec331031de644320927800375b9f84f6e143
messense <messense@icloud.com>
2014-06-10 19:15:16 +0800

    Read requirements.txt for setup install_requires, keywords and classifiers added for PyPi

--- setup.py
+++ setup.py
@@ -10,13 +10,7 @@ entry_points = {
     ]
 }
 
-requires = [
-    "Markdown",
-    "Pygments",
-    "Jinja2",
-    "PyYAML",
-    "docopt",
-]
+requires = open("requirements.txt").readlines()
 
 setup(
     name = "simiki",
@@ -25,9 +19,20 @@ setup(
     author = "Tanky Woo",
     author_email = "me@tankywoo.com",
     description = "Simiki is a simple wiki framework, written in Python.",
+    keywords = "simiki, wiki, generator",
     license = "MIT License",
     packages = find_packages(),
     include_package_data=True,
     install_requires = requires,
     entry_points = entry_points,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    ],
 )
