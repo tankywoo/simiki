005eb5e786d165f9b71dd20642bc519c1279e0a6
Tanky Woo <wtq1990@gmail.com>
2015-08-31 22:46:38 +0800

    rename mywiki to mywiki_for_cli, rm _config.yml, fabfile, themes

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -10,7 +10,7 @@ import io
 from copy import deepcopy
 
 from simiki import cli
-from simiki.utils import emptytree
+from simiki.utils import copytree, emptytree
 
 TESTS_ROOT = os.path.abspath(os.path.dirname(__file__))
 
@@ -70,10 +70,25 @@ class TestCliInit(unittest.TestCase):
 class TestCliGenerate(unittest.TestCase):
     def setUp(self):
         self.args = deepcopy(INIT_ARGS)
-        self.target_path = os.path.join(TESTS_ROOT, "mywiki")
-        self.output_path = os.path.join(self.target_path, "output")
+        test_path = os.path.dirname(os.path.abspath(__file__))
+        base_path = os.path.dirname(test_path)
+        self.wiki_path = os.path.join(test_path, "mywiki_for_cli")
+        self.output_path = os.path.join(self.wiki_path, "output")
+
         if os.path.exists(self.output_path):
             emptytree(self.output_path)
+
+        config_file_tpl = os.path.join(base_path, 'simiki',
+                                       'conf_templates', '_config.yml.in')
+        self.config_file_dst = os.path.join(self.wiki_path, '_config.yml')
+        shutil.copyfile(config_file_tpl, self.config_file_dst)
+
+        s_themes_path = os.path.join(base_path, 'simiki', 'themes')
+        self.d_themes_path = os.path.join(self.wiki_path, 'themes')
+        if os.path.exists(self.d_themes_path):
+            shutil.rmtree(self.d_themes_path)
+        copytree(s_themes_path, self.d_themes_path)
+
         self.drafts = [
             os.path.join(self.output_path, "intro", "my_draft.html")
         ]
@@ -92,27 +107,30 @@ class TestCliGenerate(unittest.TestCase):
         self.static = [
             os.path.join(self.output_path, "static", "css", "style.css"),
         ]
-        os.chdir(self.target_path)
+        os.chdir(self.wiki_path)
 
     def test_generate(self):
         self.args.update({u'generate': True})
         cli.execute(self.args)
         for f in self.drafts:
-            self.assertFalse(os.path.isfile(os.path.join(self.target_path, f)))
+            self.assertFalse(os.path.isfile(os.path.join(self.wiki_path, f)))
 
         for f in self.files:
-            self.assertTrue(os.path.isfile(os.path.join(self.target_path, f)))
+            self.assertTrue(os.path.isfile(os.path.join(self.wiki_path, f)))
 
         for d in self.dirs:
-            self.assertTrue(os.path.isdir(os.path.join(self.target_path, d)))
+            self.assertTrue(os.path.isdir(os.path.join(self.wiki_path, d)))
 
         for f in self.attach:
-            self.assertTrue(os.path.isdir(os.path.join(self.target_path, d)))
+            self.assertTrue(os.path.isdir(os.path.join(self.wiki_path, d)))
 
         for f in self.static:
-            self.assertTrue(os.path.isdir(os.path.join(self.target_path, d)))
+            self.assertTrue(os.path.isdir(os.path.join(self.wiki_path, d)))
 
     def tearDown(self):
+        os.remove(self.config_file_dst)
+        if os.path.exists(self.d_themes_path):
+            shutil.rmtree(self.d_themes_path)
         if os.path.exists(self.output_path):
             emptytree(self.output_path)
 
