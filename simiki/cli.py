fd7a060734065cf14b78791c088199063bf86109
Tanky Woo <wtq1990@gmail.com>
2015-11-29 01:07:03 +0800

    Add update subcommand to update builtin scripts and themes

--- simiki/cli.py
+++ simiki/cli.py
@@ -9,6 +9,7 @@ Usage:
   simiki new | n -t <title> -c <category> [-f <file>]
   simiki generate | g
   simiki preview | p [--host <host>] [--port <port>] [-w]
+  simiki update
   simiki -h | --help
   simiki -V | --version
 
@@ -37,6 +38,7 @@ import traceback
 import random
 import multiprocessing
 import time
+import hashlib
 
 from docopt import docopt
 from yaml import YAMLError
@@ -123,6 +125,74 @@ def preview_site(host, port, dest, root, do_watch):
     except (KeyboardInterrupt, SystemExit):
         # manually terminate process?
         pass
+
+
+def update_builtin():
+    '''Update builtin scripts and themes under local site'''
+    # for fabfile.py
+    yes_ans = ('y', 'yes')
+    _fabfile_r = os.path.join(os.path.dirname(__file__), 'conf_templates',
+                              'fabfile.py')
+    _fabfile_l = os.path.join(os.getcwd(), 'fabfile.py')
+    if os.path.exists(_fabfile_l):
+        with open(_fabfile_r, 'r') as _fd:
+            _fabfile_r_md5 = hashlib.md5(_fd.read()).hexdigest()
+        with open(_fabfile_l, 'r') as _fd:
+            _fabfile_l_md5 = hashlib.md5(_fd.read()).hexdigest()
+        if _fabfile_l_md5 != _fabfile_r_md5:
+            try:
+                _ans = raw_input('Overwrite fabfile.py? (y/N) ')
+                if _ans.lower() in yes_ans:
+                    shutil.copy2(_fabfile_r, _fabfile_l)
+            except (KeyboardInterrupt, SystemExit):
+                print()  # newline with Ctrl-C
+    else:
+        try:
+            _ans = raw_input('New fabfile.py? (y/N) ')
+            if _ans.lower() in yes_ans:
+                shutil.copy2(_fabfile_r, _fabfile_l)
+        except (KeyboardInterrupt, SystemExit):
+            print()
+
+    # for themes
+    _themes_r = os.path.join(os.path.dirname(__file__), 'themes')
+    _themes_l = os.path.join(os.getcwd(), config['themes_dir'])
+    for theme in os.listdir(_themes_r):
+        _theme_r = os.path.join(_themes_r, theme)
+        _theme_l = os.path.join(_themes_l, theme)
+        if os.path.exists(_theme_l):
+            _need_update = False
+            for root, dirs, files in os.walk(_theme_r):
+                files = [f for f in files if not f.startswith(".")]
+                dirs[:] = [d for d in dirs if not d.startswith(".")]
+                for filename in files:
+                    with open(os.path.join(root, filename), 'r') as _fd:
+                        _theme_r_md5 = hashlib.md5(_fd.read()).hexdigest()
+                    _dir = os.path.relpath(root, _theme_r)
+                    with open(os.path.join(_theme_l, _dir, filename),
+                              'r') as _fd:
+                        _theme_l_md5 = hashlib.md5(_fd.read()).hexdigest()
+                    if _theme_l_md5 != _theme_r_md5:
+                        _need_update = True
+                        break
+                if _need_update:
+                    break
+            if _need_update:
+                try:
+                    _ans = raw_input('Overwrite theme {0}? (y/N) '
+                                     .format(theme))
+                    if _ans.lower() in yes_ans:
+                        shutil.rmtree(_theme_l)
+                        copytree(_theme_r, _theme_l)
+                except (KeyboardInterrupt, SystemExit):
+                    print()
+        else:
+            try:
+                _ans = raw_input('New theme {0}? (y/N) '.format(theme))
+                if _ans.lower() in yes_ans:
+                    copytree(_theme_r, _theme_l)
+            except (KeyboardInterrupt, SystemExit):
+                print()
 
 
 def method_proxy(cls_instance, method_name, *args, **kwargs):
@@ -307,6 +377,8 @@ def execute(args):
         args['--port'] = int(args['--port'])
         preview_site(args['--host'], args['--port'], config['destination'],
                      config['root'], args['-w'])
+    elif args["update"]:
+        update_builtin()
     else:
         # docopt itself will display the help info.
         pass
