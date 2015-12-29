a2d97607416ae794f1cbffb177a2445db1448575
Tanky Woo <wtq1990@gmail.com>
2015-12-30 00:29:46 +0800

    fix: jinja2.exceptions.TemplateNotFound blocked with multiprocessing
    But I do not found the reason...
    
        Exception in thread Thread-3:
        Traceback (most recent call last):
          File "/path/to/python2.7/threading.py", line 810, in __bootstrap_inner
    	self.run()
          File "/path/to/python2.7/threading.py", line 763, in run
    	self.__target(*self.__args, **self.__kwargs)
          File "/path/to/python2.7/multiprocessing/pool.py", line 389, in _handle_results
    	task = get()
        TypeError: ('__init__() takes at least 2 arguments (1 given)', <class 'jinja2.exceptions.TemplateNotFound'>, ())

--- simiki/generators.py
+++ simiki/generators.py
@@ -11,6 +11,7 @@ import os
 import os.path
 import io
 import copy
+import traceback
 try:
     from collections import OrderedDict
 except ImportError:
@@ -67,9 +68,12 @@ class PageGenerator(BaseGenerator):
         try:
             template = self.env.get_template(template_file)
             html = template.render(template_vars)
-        except TemplateError as e:
-            e.extra_msg = "unable to load template '{0}'".format(template_file)
-            raise
+        except TemplateError:
+            # jinja2.exceptions.TemplateNotFound will get blocked
+            # in multiprocessing?
+            exc_msg = "unable to load template '{0}'\n{1}" \
+                      .format(template_file, traceback.format_exc())
+            raise Exception(exc_msg)
 
         return html
 
