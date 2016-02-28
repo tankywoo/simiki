a9cf41bdcc829a42c4e16e62d221ee1014e211fd
Tanky Woo <wtq1990@gmail.com>
2016-02-28 22:11:42 +0800

    Separate update builtin dir and fix file of dir not exists error

--- simiki/updater.py
+++ simiki/updater.py
@@ -47,10 +47,58 @@ def _update_file(filename, local_path, original_path):
         logger.error(e)
 
 
+def _update_dir(dirname, local_dir, original_dir, tag='directory'):
+    '''Update sth on a per-directory basis, such as theme
+    :dirname: directory name to be updated, without parent path
+    :local_path: full path of local dirname
+    :original_path: full path of original dirname
+    :tag: input help information
+    '''
+    try:
+        if os.path.exists(local_dir):
+            _need_update = False
+            for root, dirs, files in os.walk(original_dir):
+                files = [f for f in files if not f.startswith(".")]
+                dirs[:] = [d for d in dirs if not d.startswith(".")]
+                rel_dir = os.path.relpath(root, original_dir)
+
+                for fn in files:
+                    with open(os.path.join(root, fn), 'rb') as _fd:
+                        original_fn_md5 = hashlib.md5(_fd.read()).hexdigest()
+                    local_fn = os.path.join(local_dir, rel_dir, fn)
+                    if not os.path.exists(local_fn):
+                        _need_update = True
+                        break
+                    with open(local_fn, 'rb') as _fd:
+                        local_fn_md5 = hashlib.md5(_fd.read()).hexdigest()
+                    if local_fn_md5 != original_fn_md5:
+                        _need_update = True
+                        break
+                if _need_update:
+                    break
+
+            if _need_update:
+                try:
+                    _ans = raw_input('Overwrite {0} {1}? (y/N) '
+                                     .format(tag, dirname))
+                    if _ans.lower() in yes_answer:
+                        shutil.rmtree(local_dir)
+                        copytree(original_dir, local_dir)
+                except (KeyboardInterrupt, SystemExit):
+                    print()
+        else:
+            try:
+                _ans = raw_input('New {0} {1}? (y/N) '.format(tag, dirname))
+                if _ans.lower() in yes_answer:
+                    copytree(original_dir, local_dir)
+            except (KeyboardInterrupt, SystemExit):
+                print()
+    except Exception as e:
+        logger.error(e)
+
+
 def update_builtin(**kwargs):
     '''Update builtin scripts and themes under local site'''
-    # for fabfile.py
-    yes_ans = ('y', 'yes')
     # for fabfile.py
     _update_file(
         'fabfile.py',
@@ -59,41 +107,9 @@ def update_builtin(**kwargs):
     )
 
     # for themes
-    _themes_r = os.path.join(os.path.dirname(__file__), 'themes')
-    _themes_l = os.path.join(os.getcwd(), kwargs['themes_dir'])
-    for theme in os.listdir(_themes_r):
-        _theme_r = os.path.join(_themes_r, theme)
-        _theme_l = os.path.join(_themes_l, theme)
-        if os.path.exists(_theme_l):
-            _need_update = False
-            for root, dirs, files in os.walk(_theme_r):
-                files = [f for f in files if not f.startswith(".")]
-                dirs[:] = [d for d in dirs if not d.startswith(".")]
-                for filename in files:
-                    with open(os.path.join(root, filename), 'rb') as _fd:
-                        _theme_r_md5 = hashlib.md5(_fd.read()).hexdigest()
-                    _dir = os.path.relpath(root, _theme_r)
-                    with open(os.path.join(_theme_l, _dir, filename),
-                              'rb') as _fd:
-                        _theme_l_md5 = hashlib.md5(_fd.read()).hexdigest()
-                    if _theme_l_md5 != _theme_r_md5:
-                        _need_update = True
-                        break
-                if _need_update:
-                    break
-            if _need_update:
-                try:
-                    _ans = raw_input('Overwrite theme {0}? (y/N) '
-                                     .format(theme))
-                    if _ans.lower() in yes_ans:
-                        shutil.rmtree(_theme_l)
-                        copytree(_theme_r, _theme_l)
-                except (KeyboardInterrupt, SystemExit):
-                    print()
-        else:
-            try:
-                _ans = raw_input('New theme {0}? (y/N) '.format(theme))
-                if _ans.lower() in yes_ans:
-                    copytree(_theme_r, _theme_l)
-            except (KeyboardInterrupt, SystemExit):
-                print()
+    original_themes = os.path.join(os.path.dirname(__file__), 'themes')
+    local_themes = os.path.join(os.getcwd(), kwargs['themes_dir'])
+    for theme in os.listdir(original_themes):
+        local_theme = os.path.join(local_themes, theme)
+        original_theme = os.path.join(original_themes, theme)
+        _update_dir(theme, local_theme, original_theme, 'theme')
