ae03a5ead8ee13cc1b48db09ba74d1a381a51118
Tanky Woo <wtq1990@gmail.com>
2016-06-24 21:09:05 +0800

    Add base python-markdown2 markdown engine support

--- simiki/parser.py
+++ simiki/parser.py
@@ -8,6 +8,11 @@ try:
 except ImportError:
     pass
 
+try:
+    import markdown2
+except ImportError:
+    pass
+
 
 class MarkupParser(object):
     """Markup Parser
@@ -92,3 +97,41 @@ class MistuneParser(object):
         html = mistune.markdown(text, renderer=renderer)
 
         return html
+
+
+class PythonMarkdown2Parser(object):
+    """Markdown Parser
+
+    Use [trentm/python-markdown2](https://github.com/trentm/python-markdown2)
+
+    Ref:
+      * [Extras](https://github.com/trentm/python-markdown2/wiki/Extras)
+
+    Todo:
+      * toc is experimental
+    """
+    def __init__(self, site_config):
+        self.site_config = site_config
+
+    def parse(self, text):
+        """Parse markup text to html"""
+        markdown_extensions = self._set_markdown_extensions()
+
+        html = markdown2.markdown(text, extras=markdown_extensions)
+
+        return html
+
+    def _set_markdown_extensions(self):
+        """Set the extensions for markdown parser"""
+        if self.site_config.get('markdown'):
+            # if markdown options defined in _config.yml, such as:
+            # markdown:
+            #   - fenced-code-blocks
+            #   - footnotes
+            #   - tables
+            markdown_extensions = self.site_config['markdown']
+        else:
+            markdown_extensions = ['fenced-code-blocks', 'footnotes',
+                                   'tables']
+
+        return markdown_extensions
